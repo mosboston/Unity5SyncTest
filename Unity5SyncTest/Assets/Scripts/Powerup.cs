dc2ee0696de60e408b7877616f180a49753da943
mosbjones <bjones@mos.org>
2015-07-16 16:12:49 -0400

    Added Powerups!

---
+++ Unity5SyncTest/Assets/Scripts/Powerup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		sound = gameObject.GetComponent<AudioSource>();
+	}
+	
+	void OnTriggerEnter(Collider col) {
+//		foreach (ContactPoint contact in collision.contacts) {
+//			Debug.DrawRay(contact.point, contact.normal, Color.white);
+//		}
+		//if (collision.relativeVelocity.magnitude > 2){
+			sound.Play();
+			Debug.Log("PLAY");
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+			StartCoroutine("RemoveMe");
+			
+		//}
+		//Destroy(gameObject);
+		
+	}
+	
+	IEnumerator RemoveMe(){
+		yield return new WaitForSeconds(1f);
+		Destroy(gameObject);
+	}
+	
+}
